139e0d5efb6ac10f4bcca672148edc4553956a92
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2020-10-03 11:06:31 +0300

    Added data not full

---
+++ data.js
@@ -0,0 +1,255 @@
+var aerozones = [
+    {
+      aerozoneName: "BAHOVO",
+      latitude: 43.206111,
+      longitude: 24.655556,
+      radius: 2.5,
+      icaoCode: "LBBA",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BLAGOEVO",
+      latitude: 43.455278,
+      longitude: 26.431667,
+      radius: 2.5,
+      icaoCode: "LBBL",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BOHOT-LM",
+      latitude: 43.306944,
+      longitude: 24.691667,
+      radius: 2.5,
+      icaoCode: "LBBO",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BELCHIN",
+      latitude: 42.374722,
+      longitude: 23.3925,
+      radius: 2.5,
+      icaoCode: "LBBB",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BYALA",
+      latitude: 43.463611,
+      longitude: 25.711667,
+      radius: 2.5,
+      icaoCode: "LBBQ",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BREZNIK",
+      latitude: 42.693333,
+      longitude: 22.896667,
+      radius: 2.5,
+      icaoCode: "LBBR",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "DASKAL ATANASOVO",
+      latitude: 42.321389,
+      longitude: 25.893611,
+      radius: 2.5,
+      icaoCode: "LBDA",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "DOLNA BANYA",
+      latitude: 42.308611,
+      longitude: 23.820556,
+      radius: 2.5,
+      icaoCode: "LBDB",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "DVE MOGILI",
+      latitude: 43.606667,
+      longitude: 25.890833,
+      radius: 2.5,
+      icaoCode: "LBDM",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "DRAGANOVTSI",
+      latitude: 42.9375,
+      longitude: 25.170833,
+      radius: 2.5,
+      icaoCode: "LBDR",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "GORSKI IZVOR",
+      latitude: 42.035833,
+      longitude: 25.408333,
+      radius: 2.5,
+      icaoCode: "LBGI",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "MONTANA",
+      latitude: 43.380556,
+      longitude: 23.265278,
+      radius: 2.5,
+      icaoCode: "LBМO",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "PRIMORSKO",
+      latitude: 42.259722,
+      longitude: 27.703611,
+      radius: 3.5,
+      icaoCode: "LBPR",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "STARO SELISHTE",
+      latitude: 43.650278,
+      longitude: 26.754167,
+      radius: 2.5,
+      icaoCode: "LBSE",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "ERDEN",
+      latitude: 43.499444,
+      longitude: 23.304444,
+      radius: 2.5,
+      icaoCode: "LBRD",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "TSALAPITSA",
+      latitude: 42.188889,
+      longitude: 24.535556,
+      radius: 2.5,
+      icaoCode: "LBTS",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "VRATSA",
+      latitude: 43.255833,
+      longitude: 23.532778,
+      radius: 2.5,
+      icaoCode: "LBVR",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BURGAS",
+      latitude: 42.570278,
+      longitude: 27.515278,
+      radius: 7.5,
+      icaoCode: "LBBG",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "BEZMER",
+      latitude: 42.454722,
+      longitude: 26.3525,
+      radius: 1.08,
+      icaoCode: "LBIA",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "DOLNA MITROPOLIA",
+      latitude: 43.451667,
+      longitude: 24.502778,
+      radius: 1.08,
+      icaoCode: "LBPD",
+      polygonType: "Circle",
+    },
+    {
+      aerozoneName: "GRAF IGNATIEVO",
+      points: [
+        [42.388889, 24.612778],
+        [42.408889, 24.763889],
+        [42.356389, 24.864444],
+        [42.282778, 24.881667],
+        [42.288333, 24.796389],
+        [42.2675, 24.604444],
+        [42.240556, 24.560278],
+        [42.314444, 24.5425],
+        [42.388889, 24.612778],
+      ],
+      icaoCode: "LBPG",
+      polygonType: "Polygon",
+    },
+    {
+      aerozoneName: "VARNA",
+      icaoCode: "LBWN",
+      polygonType: "ArcSector",
+      points: {
+        center: [43.231944, 27.825278],
+        all: [[43.235000, 27.996389],[43.265000, 27.660278],[43.231944, 27.825278]]
+      },
+      startAngle: 88.5,
+      endAngle: 285.5,
+      radius: 7.5,
+    },
+    {
+      aerozoneName: "BAZAN",
+      icaoCode: "LBBZ",
+      polygonType: "ArcSector",
+      points: {
+        center: [43.728611, 26.119444],
+        all: [[43.728611, 26.119444], [43.690000,26.141111], [43.728889, 26.104444], [43.762222, 26.085556]]
+      },
+      startAngle: 324,
+      endAngle: 158,
+      radius: 2.5,
+    },
+    {
+      aerozoneName: "PLOVDIV",
+      icaoCode: "LBPD",
+      polygonType: "ArcSector",
+      points: {
+        center: [42.067778, 24.850833],
+        all: [[42.067778, 24.850833], [41.976389, 24.965278], [42.128889, 24.704167]]
+      },
+      startAngle: 300,
+      endAngle: 137,
+      radius: 7.5,
+    },
+    {
+      aerozoneName: "GORNA ORYAHOTITSA",
+      icaoCode: "LBGO",
+      polygonType: "ArcSector",
+      points: {
+        center: [43.151667, 25.711944],
+        all: [[43.116667, 25.875833], [43.151667, 25.711944], [43.151389, 25.541111]]
+      },
+      startAngle: 270,
+      endAngle: 106.2,
+      radius: 7.5,
+    },
+    {
+      aerozoneName: "SOFIA",
+      icaoCode: "LBSF",
+      polygonType: "ArcSector",
+      points: {
+        center: [42.695000, 23.408333],
+        all: [[42.695000, 23.408333], [42.669444, 23.574167], [42.688611, 23.239167]]
+      },
+      startAngle: 267,
+      endAngle: 102,
+      radius: 7.5,
+    },
+  ];
+  
+  //special once
+  //SOFIA
+  //BALCHIK
+  //STUDEN KLADENETS
+  //SOFIA WEST
+  //SLIVNITSA
+  //SLANCHEV BRYAG
+  //GRADISHTE
+  //GRIVITSA
+  //KAINARDJA
+  //KAZANLAK
+  //IHTIMAN
+  //MARITSA
+  //LESNOVO
+  //RUSE
+  
